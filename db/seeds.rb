ef620498116678c309e39343bc8dbbacee845965
ronflow <ronieflorezbkp@gmail.com>
2025-05-11 22:32:12 -0300

    Render updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,118 @@
 # db/seeds.rb
 
   
-  Event.create!(event_date: "2025-05-15", locale: "Gaz Burning", description: "Rock Flowerz no Gaz - Quinta dia 15/Mai")
-  Event.create!(event_date: "2025-05-29", locale: "Gaz Burning", description: "Rock Flowerz no Gaz - Quinta dia 29/Mai")
+  # Event.create!(event_date: "2025-05-15", locale: "Gaz Burning", description: "Rock Flowerz no Gaz - Quinta dia 15/Mai")
+  # Event.create!(event_date: "2025-05-29", locale: "Gaz Burning", description: "Rock Flowerz no Gaz - Quinta dia 29/Mai")
   
+  Song.create!( name: "Freak", artist: "", tags: "rock; internacional", lyrics: "[DROP]
+
+
+Tom: 
+Intro 4x: (Riff 1)
+
+ E  DEE  EEE DDD
+
+No more maybes
+Your baby`s got rabies
+Sitting on a  ball
+In the middle of the andes
+
+Refrão:
+
+D   F Gb    (G Ab )
+
+   Yeah
+I`m a freak of nature
+   Yeah
+I`m a freak
+
+   F7M(11+)
+If only I could be
+
+ E  DEE  EEE D E F Gb
+As cool as you
+As cool as you
+
+[D     E F Gb G]
+Segunda Parte:
+(Riff 4)
+Body and soul, I`m a freak, I`m a freak
+Body and soul, I`m a freak, I`m a freak
+
+Riff 4 - Parte 1 de 2            (3x)
+F5      C5     D5
+D|----3-------5------5---4---2---0-----
+A|--3---3---3------0-------------------
+E|3-------x------0-----0---0---0-------
+
+Riff 4 - Parte 2 de 2
+F5      C5     D5
+D|----3-------5--0---------------------
+A|--3---3---3----0---------------------
+E|3-------x------0---------------------
+
+
+(Riff 5)
+
+Primeira Parte com variação na letra:
+
+(Riff 1)
+ E  DEE  EEE DDD
+Try to be different
+Well get a different disease
+Seems it`s in fashion
+To need the cold sore cream
+
+
+D   F Gb    (G Ab )
+Yeah
+I`m a freak of nature
+Yeah
+I`m a freak
+
+(Riff 5)
+
+G5             F# 
+I don`t really know
+How t`put`on a cool show
+As boring as they come
+Just tell me where to  go
+   F7M(11+)
+If only I could be as cool as you
+
+(Riff 3)
+ E  DEE  EEE D E F Gb
+As cool as you
+As cool as you
+As cool as you
+As cool as you
+
+Segunda Parte com variação:
+(Riff 6)
+F        C  D           F          C D
+
+Body and soul, I`m a freak, I`m a freak
+Body and soul, I`m a freak, I`m a freak
+
+Solo
+
+
+
+Solo 2x triplets
+E|------------------------------------------
+B|10-10-10-10-10-13-13-13-13-10-------------
+G|-------------------------------11-11-11-11 ….
+
+Solo - Parte 3 de 3
+E|------------------------------
+B|/12...\7.../10...\7...\-------
+G|------------------------------
+
+F        C  D           F          C D
+Body and soul, I`m a freak, I`m a freak
+Body and soul, I`m a freak, I`m a freak
+
+(Riff 1) (4x)
+Freak
+
+E dim")
