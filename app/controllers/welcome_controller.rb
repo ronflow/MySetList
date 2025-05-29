6fb570317180ccf42d40e5f212209e994cf3616a
ronflow <ronieflorezbkp@gmail.com>
2025-05-29 10:50:34 -0300

    Atualizar filtro data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @events = Event.order(:event_date)
+    # @events = Event.order(:event_date)
+    @events = Event.where("event_date >= ?", Date.yesterday).order(:event_date)
+    
   end
 end
