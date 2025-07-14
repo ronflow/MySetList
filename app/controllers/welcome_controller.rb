c5320a355d4130ca07d255fbb833394ad4bd55ee
VitorV1torV170R <vitor.oliveiracosta94@gmail.com>
2025-07-14 16:15:27 -0300

    Novos arquivos artista, autenticacao e atualizacoes 3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,11 +5,15 @@ class WelcomeController < ApplicationController
   def index
     @events = Event.where("event_date >= ?", Date.yesterday).order(:event_date)
     
-    if current_user
+    if params[:artist_name].present?
+      @artist = Artist.find_by(name: params[:artist_name])
+      # Se o parâmetro existe mas o artist não foi encontrado, usar 'Rock Flowerz'
+      @artist = Artist.find_by(name: 'Rock Flowerz') if @artist.nil?
+    elsif current_user
       @artist = current_user.artists.first
     else
       # Se não há usuário logado, usar artist ID 11
-      @artist = Artist.find_by(id: 11)
+      @artist = Artist.find_by(name: 'Rock Flowerz')
     end
   end
 end
